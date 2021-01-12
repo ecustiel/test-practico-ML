9638fd4f3c463cae7d3299fb7c05a7ec2f87d34e
EduardoCustiel <ecustiel@gmail.com>
2021-01-11 21:12:58 -0300

    Primer Actualizacion 11/1

---
+++ src/components/ResultsScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ResultsScreen = () => {
+    return (
+        <div>
+            <h1>Resultados Screen</h1>
+        </div>
+    )
+}
---
+++ src/router/AppRouter.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
+  } from "react-router-dom";
+import {SearchScreen} from './components/SearchScreen';
+import {ResultsScreen} from './components/ResultsScreen';
+import {DetailsScreen} from './components/DetailsScreen';
+
+
+
+
+export const AppRouter = () => {
+    return (
+        <Router>
+        <div>
+            <Switch>
+                <Route exact path='/' component={ SearchScreen } />
+                <Route exact path='/items?search=' component={ ResultsScreen} />
+                <Route exact path='/items/:id' component={ DetailsScreen } />
+                
+
+                <Redirect to='/' />
+            </Switch>  
+        </div>
+        </Router>
+    )
+}
